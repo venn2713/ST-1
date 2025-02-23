3c0c6097cdf874e888fd112d925d67b090501a63
Sergey Vasilev <112872559+venn2713@users.noreply.github.com>
2025-02-23 23:08:17 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,17 +1,40 @@
-// Copyright 2025 UNN-CS
+// Copyright 2025 VasilevSA
 
 #include <gtest/gtest.h>
 #include <cstdint>
 #include "alg.h"
 
-
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(CheckPrimeTest, BasicCases) {
+    EXPECT_TRUE(checkPrime(2));
+    EXPECT_TRUE(checkPrime(3));
+    EXPECT_TRUE(checkPrime(5));
+    EXPECT_TRUE(checkPrime(13));
+    EXPECT_FALSE(checkPrime(1));
+    EXPECT_FALSE(checkPrime(0));
+    EXPECT_FALSE(checkPrime(4));
+    EXPECT_FALSE(checkPrime(9));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(NPrimeTest, BasicCases) {
+    EXPECT_EQ(nPrime(1), 2);
+    EXPECT_EQ(nPrime(2), 3);
+    EXPECT_EQ(nPrime(3), 5);
+    EXPECT_EQ(nPrime(4), 7);
+    EXPECT_EQ(nPrime(5), 11);
+    EXPECT_EQ(nPrime(10), 29);
+}
+
+TEST(NextPrimeTest, BasicCases) {
+    EXPECT_EQ(nextPrime(2), 3);
+    EXPECT_EQ(nextPrime(3), 5);
+    EXPECT_EQ(nextPrime(11), 13);
+    EXPECT_EQ(nextPrime(20), 23);
+}
+
+TEST(SumPrimeTest, BasicCases) {
+    EXPECT_EQ(sumPrime(2), 0);
+    EXPECT_EQ(sumPrime(3), 2);
+    EXPECT_EQ(sumPrime(5), 10); // 2 + 3 + 5
+    EXPECT_EQ(sumPrime(10), 17); // 2 + 3 + 5 + 7
+    EXPECT_EQ(sumPrime(20), 77); // 2+3+5+7+11+13+17+19
 }
