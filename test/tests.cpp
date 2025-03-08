5ccb0d9023130489542c8669cbcf63744adfdb6d
Sergey Vasilev <112872559+venn2713@users.noreply.github.com>
2025-03-08 12:58:03 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,37 +4,29 @@
 #include <cstdint>
 #include "alg.h"
 
-TEST(CheckPrimeTest, BasicCases) {
-    EXPECT_TRUE(checkPrime(2));
-    EXPECT_TRUE(checkPrime(3));
-    EXPECT_TRUE(checkPrime(5));
-    EXPECT_TRUE(checkPrime(13));
-    EXPECT_FALSE(checkPrime(1));
-    EXPECT_FALSE(checkPrime(0));
-    EXPECT_FALSE(checkPrime(4));
-    EXPECT_FALSE(checkPrime(9));
-}
+TEST(CheckPrimeTest, Prime2) { EXPECT_TRUE(checkPrime(2)); }
+TEST(CheckPrimeTest, Prime3) { EXPECT_TRUE(checkPrime(3)); }
+TEST(CheckPrimeTest, Prime5) { EXPECT_TRUE(checkPrime(5)); }
+TEST(CheckPrimeTest, Prime13) { EXPECT_TRUE(checkPrime(13)); }
+TEST(CheckPrimeTest, NotPrime1) { EXPECT_FALSE(checkPrime(1)); }
+TEST(CheckPrimeTest, NotPrime0) { EXPECT_FALSE(checkPrime(0)); }
+TEST(CheckPrimeTest, NotPrime4) { EXPECT_FALSE(checkPrime(4)); }
+TEST(CheckPrimeTest, NotPrime9) { EXPECT_FALSE(checkPrime(9)); }
 
-TEST(NPrimeTest, BasicCases) {
-    EXPECT_EQ(nPrime(1), 2);
-    EXPECT_EQ(nPrime(2), 3);
-    EXPECT_EQ(nPrime(3), 5);
-    EXPECT_EQ(nPrime(4), 7);
-    EXPECT_EQ(nPrime(5), 11);
-    EXPECT_EQ(nPrime(10), 29);
-}
+TEST(NPrimeTest, First) { EXPECT_EQ(nPrime(1), 2); }
+TEST(NPrimeTest, Second) { EXPECT_EQ(nPrime(2), 3); }
+TEST(NPrimeTest, Third) { EXPECT_EQ(nPrime(3), 5); }
+TEST(NPrimeTest, Fourth) { EXPECT_EQ(nPrime(4), 7); }
+TEST(NPrimeTest, Fifth) { EXPECT_EQ(nPrime(5), 11); }
+TEST(NPrimeTest, Tenth) { EXPECT_EQ(nPrime(10), 29); }
 
-TEST(NextPrimeTest, BasicCases) {
-    EXPECT_EQ(nextPrime(2), 3);
-    EXPECT_EQ(nextPrime(3), 5);
-    EXPECT_EQ(nextPrime(11), 13);
-    EXPECT_EQ(nextPrime(20), 23);
-}
+TEST(NextPrimeTest, After2) { EXPECT_EQ(nextPrime(2), 3); }
+TEST(NextPrimeTest, After3) { EXPECT_EQ(nextPrime(3), 5); }
+TEST(NextPrimeTest, After11) { EXPECT_EQ(nextPrime(11), 13); }
+TEST(NextPrimeTest, After20) { EXPECT_EQ(nextPrime(20), 23); }
 
-TEST(SumPrimeTest, BasicCases) {
-    EXPECT_EQ(sumPrime(2), 0);
-    EXPECT_EQ(sumPrime(3), 2);
-    EXPECT_EQ(sumPrime(5), 5);
-    EXPECT_EQ(sumPrime(10), 17);
-    EXPECT_EQ(sumPrime(20), 77);
-}
+TEST(SumPrimeTest, UpTo2) { EXPECT_EQ(sumPrime(2), 0); }
+TEST(SumPrimeTest, UpTo3) { EXPECT_EQ(sumPrime(3), 2); }
+TEST(SumPrimeTest, UpTo5) { EXPECT_EQ(sumPrime(5), 5); }
+TEST(SumPrimeTest, UpTo10) { EXPECT_EQ(sumPrime(10), 17); }
+TEST(SumPrimeTest, UpTo20) { EXPECT_EQ(sumPrime(20), 77); }
